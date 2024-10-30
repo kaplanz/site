2592024994dc00b61cb8f5f0fb219ce0885f13d9
Zakhary Kaplan <me@zakhary.dev>
2024-10-30 00:00:00 -0400

    feat: initial upload

---
+++ src/pages/blog/feed.xml.js
@@ -0,0 +1,33 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+import sanitizeHtml from "sanitize-html";
+
+import { title, about as description } from "./index.astro";
+
+export async function GET(context) {
+  // Collect sorted posts
+  const posts = (await getCollection("blog")).sort(
+    (a, b) => b.data.created.valueOf() - a.data.created.valueOf(),
+  );
+  return rss({
+    // `<title>` field in output xml
+    title,
+    // `<description>` field in output xml
+    description,
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: context.site,
+    // Render RSS items with(out) a trailing slash
+    trailingSlash: false,
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: posts.map((post) => ({
+      link: `/blog/${post.slug}`,
+      pubDate: post.data.created,
+      title: post.data.title,
+      description: post.data.description.trim(),
+      content: sanitizeHtml(post.rendered?.html),
+    })),
+  });
+}
