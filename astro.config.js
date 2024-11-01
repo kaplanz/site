5af091e90717bcdf0654dbba78e2331c52734bff
Zakhary Kaplan <me@zakhary.dev>
2024-11-01 00:00:00 -0400

    feat(markdown): add header anchors

--- astro.config.js
+++ astro.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 import markdown from "@astropub/md";
 import sitemap from "@astrojs/sitemap";
 
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+
 import remarkCallouts from "remark-callouts";
 import remarkReadingTime from "remark-reading-time";
 import remarkSidenotes from "remark-sidenotes";
@@ -13,6 +16,19 @@ export default defineConfig({
   integrations: [markdown(), sitemap()],
   publicDir: "src/static",
   markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: "anchor",
+            rel: "nofollow",
+          },
+        },
+      ],
+    ],
     remarkPlugins: [remarkCallouts, remarkReadingTime, remarkSidenotes],
     shikiConfig: {
       themes: {
