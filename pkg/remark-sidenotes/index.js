c64ecf2a8fa31567f1b5fea7c18cb36624ae6b9f
Zakhary Kaplan <me@zakhary.dev>
2024-11-01 00:00:00 -0400

    feat(markdown): impl remark-sidenotes

---
+++ pkg/remark-sidenotes/index.js
@@ -0,0 +1,65 @@
+import { visit } from "unist-util-visit";
+import { u } from "unist-builder";
+
+export default function remarkSidenotes() {
+  return (tree) => {
+    visit(tree, "footnoteDefinition", (def) => {
+      // Replace footnote references with sidenotes
+      visit(tree, "footnoteReference", (ref, index, parent) => {
+        // Only modify reference for this definition
+        if (ref.identifier !== def.identifier) return;
+
+        // Insert footnote reference
+        parent.children.splice(
+          index,
+          1,
+          u(
+            "containerDirective",
+            {
+              data: {
+                hName: "span",
+                hProperties: {
+                  className: "sidenote",
+                },
+              },
+            },
+            [
+              u(
+                "text",
+                {
+                  data: {
+                    hName: "label",
+                    hProperties: {
+                      for: `sn-toggle-${def.identifier}`,
+                    },
+                  },
+                },
+                "",
+              ),
+              u(
+                "text",
+                {
+                  data: {
+                    hName: "input",
+                    hProperties: {
+                      id: `sn-toggle-${def.identifier}`,
+                      type: "checkbox",
+                    },
+                  },
+                },
+                "",
+              ),
+              u(
+                "containerDirective",
+                {
+                  data: { hName: "span" },
+                },
+                def.children?.[0]?.children,
+              ),
+            ],
+          ),
+        );
+      });
+    });
+  };
+}
