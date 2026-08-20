2c5f8c146dde8cfc19d45ecbed0293bc04b1b560
Zakhary Kaplan <me@zakhary.dev>
2026-08-19 20:00:00 -0400

    feat(games): add nested navbar

---
+++ src/srv/media/games.11tydata.js
@@ -0,0 +1,9 @@
+export default {
+  eleventyComputed: {
+      games: (data) => data.fetch.media.game
+        .filter(game => game.logs?.length)
+        .sort((a, b) =>
+          (b.logs?.at(-1)?.date ?? 0) - (a.logs?.at(-1)?.date ?? 0)
+        )
+  }
+};
